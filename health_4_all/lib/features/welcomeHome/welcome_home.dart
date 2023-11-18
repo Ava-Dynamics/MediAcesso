a8b1d6f4931c2ce948ded934b3fcdf3f1a5778c4
ThaisaFujii <71739802+ThaisaFujii@users.noreply.github.com>
2023-11-18 19:55:39 -0300

    Editando welcomePage
    feito alguns ajustes de texto e espaçamento

---
+++ health_4_all/lib/features/welcomeHome/welcome_home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:health_4_all/features/welcomeHome/welcome_home_view.dart';
+import 'package:health_4_all/features/welcomeHome/welcome_home_view_model.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChangeNotifierProvider(
+        create: (context) => WelcomeHomeViewModel(),
+        child: WelcomeHomePage(),
+      ),
+    );
+  }
+}
+
+class WelcomeHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    WelcomeHomeViewModel viewModel = Provider.of<WelcomeHomeViewModel>(context);
+
+    return WelcomeHomeView(
+      onQueroMeCadastrarPressed: viewModel.onQueroMeCadastrarPressed,
+      onJaSouCadastradoPressed: viewModel.onJaSouCadastradoPressed,
+    );
+  }
+}
--- health_4_all/lib/main.dart
+++ health_4_all/lib/main.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:health_4_all/features/welcomeHome/welcome_home.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
   await Firebase.initializeApp();
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
